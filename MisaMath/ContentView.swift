9efcb7db20f3c08175a228526fed1b2e46026cf2
Tomas Vesely <tomves@outlook.com>
2020-02-08 20:29:00 -0800

    Hello World

---
+++ MisaMath/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MisaMath
+//
+//  Created by Tomáš Veselý on 2/8/20.
+//  Copyright © 2020 Tomáš Veselý. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
